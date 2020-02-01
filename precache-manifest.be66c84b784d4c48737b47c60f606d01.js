1cdcfbc80241509a5c4fee6656c4687e0ab3ba22
Pavel <fialkopavel@gmail.com>
2020-02-01 15:55:49 +0200

    Updates

---
+++ precache-manifest.be66c84b784d4c48737b47c60f606d01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0cff76c3bd6065552f8f61eed4cb10a",
+    "url": "/StarWars-API/index.html"
+  },
+  {
+    "revision": "b1424126fe1d7a8abec0",
+    "url": "/StarWars-API/static/js/2.2bdc32f6.chunk.js"
+  },
+  {
+    "revision": "a231e00e40e54a7050cc",
+    "url": "/StarWars-API/static/js/main.af9924b8.chunk.js"
+  },
+  {
+    "revision": "1a1adc43854453633f5e",
+    "url": "/StarWars-API/static/js/runtime-main.7647ccc5.js"
+  },
+  {
+    "revision": "1f0106732607a5b5c6b8be977c01a285",
+    "url": "/StarWars-API/static/media/ErrorImage.1f010673.png"
+  }
+]);
